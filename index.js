4636245e2301a21a789539335cde390da641d751
jemadari <hassanjemadari@gmail.com>
2025-09-04 14:06:42 +0300

    Practice on how to use Global Variables with Nodejs

--- index.js
+++ index.js
@@ -1,26 +1,41 @@
-const http = require("http");
+// ########################################
+//  Handling directories in NodeJs
+// ########################################
 
-const server = http.createServer((req, res) => {
-    if(req.url === "/")
-    {
-        res.writeHead(200, {"content-type": "text/html"})
-        res.end("<h1>This is the home page</h1> <p>Please Press here <a href='/old-page'>Old Page</a> </p>")
-    }
-    else if(req.url === "/old-page")
-    {
-        res.writeHead(301, {location: "/new-page"})
-        res.end()
-    }
-    else if(req.url === "/new-page")
-    {
-        res.writeHead(200, {"content-type": "text/html"})
-        res.end("<h1>This is the New page</h1>");
-    }
-    else {
-        res.writeHead(404, {"content-type" : "text/html"})
-        res.end("<h1>Page not found!!!</h1>")
-    }
+// const http = require("http");
 
-}).listen(3000, () => {
-    console.log("Server Started at port 3000")
-})
+// const server = http.createServer((req, res) => {
+//     if(req.url === "/")
+//     {
+//         res.writeHead(200, {"content-type": "text/html"})
+//         res.end("<h1>This is the home page</h1> <p>Please Press here <a href='/old-page'>Old Page</a> </p>")
+//     }
+//     else if(req.url === "/old-page")
+//     {
+//         res.writeHead(301, {location: "/new-page"})
+//         res.end()
+//     }
+//     else if(req.url === "/new-page")
+//     {
+//         res.writeHead(200, {"content-type": "text/html"})
+//         res.end("<h1>This is the New page</h1>");
+//     }
+//     else {
+//         res.writeHead(404, {"content-type" : "text/html"})
+//         res.end("<h1>Page not found!!!</h1>")
+//     }
+
+// }).listen(3000, () => {
+//     console.log("Server Started at port 3000")
+// })
+
+// ########################################
+// This section is about Global Variable
+// ########################################
+// global.appName = "Maarifa";
+
+// require("./other");
+
+// console.log(__dirname); // this used to show the current working directory
+
+// console.log(__filename); // this used to show the current working file
---
+++ other.js
@@ -0,0 +1,2 @@
+// am using appName as a global variable from index.js
+console.log("This is Our App Name: ", appName)
