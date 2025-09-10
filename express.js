b3586c4228fd01d13152555f2531169e134d7a69
jemadari <hassanjemadari@gmail.com>
2025-09-10 10:18:50 +0300

    Implementing json middleware as well as POST Request method

--- express.js
+++ express.js
@@ -1,68 +1,70 @@
-import express from "express";
+import express, { response } from "express";
 
 const app = express();
 
+// json middleware
+app.use(express.json())
+
 const PORT = process.env.PORT || 3000
 
-app.get("/", (request, response) => {
-    response.send("This is the root '/' page");
-})
+const users = [
+    {id: 1, username: "Hancy", age: "19"},
+    {id: 2, username: "Lancy", age: "23"},
+    {id: 3, username: "Vancy", age: "54"},
+    {id: 4, username: "Dancy", age: "24"},
+    {id: 5, username: "Liam", age: "30"},
+    {id: 6, username: "Smith", age: "39"},
+]
 
 const products = [
-    {id: 1, name: "Juice"},
-    {id: 2, name: "Water"},
-    {id: 3, name: "Cake"},
-    {id: 4, name: "Pen"},
-    {id: 5, name: "Pencil"},
+    {id: 1, name: "Juice", price: "3$"},
+    {id: 2, name: "Mango", price: "2$"},
+    {id: 3, name: "Apple", price: "5$"},
+    {id: 4, name: "Octopus", price: "50$"},
+    {id: 5, name: "Bread", price: "4$"},
+    {id: 6, name: "Milk", price: "10$"},
 ]
 
-const users = [
-    {id: 1, username: "Percy Tau"},
-    {id: 2, username: "Zubimendi"},
-    {id: 3, username: "Matheo"},
-    {id: 4, username: "Raya"},
-    {id: 5, username: "Eze"},
-]
-
-app.get("/api/products", (request, response) => {
-    response.status(200).send(products);
-})
-
-// Hii ni namna ya kutumia filter function ili kuweza kufilter data based on your need
-// app.get("/api/products", (request, response) => {
-//     const prod = products.filter((product) => product.name === "Pen")
-//     response.status(200).send(prod);
-// })
-
-app.get("/api/product/:id", (request, response) => {
-    const parsedId = parseInt(request.params.id);
-    if(isNaN(parsedId))
-        return response.status(400).send("Invalid Id");
-
-    const product = products.find((product) => product.id === parsedId);
-
-    if(!product)
-        return response.status(404).send(`The product with ${parsedId} id is not found`);
-    return response.status(200).send(product);
+app.get("/", (request, response) => {
+    response.status(200).send([{
+        message: "This is the root page"
+    }])
 })
 
 app.get("/api/users", (request, response) => {
-    response.status(200).send(users);
+    response.send(users);
 })
 
 app.get("/api/user/:id", (request, response) => {
-    const parsedId = parseInt(request.params.id);
-    
-    if(isNaN(parsedId))
-        return response.status(400).send("Invalid User Id");
+    const parsedId = parseInt(request.params.id)
 
-    const user = users.find((user) => user.id === parsedId)
+    if(isNaN(parsedId))
+        return response.status(400).send([{message: "Bad Request, Invalid Id"}]);
+
+    const user = users.find((user) => user.id === parsedId);
     if(!user)
         return response.sendStatus(404)
 
-    return response.status(200).send(user);
+    return response.status(200).send(user)
+})
+
+app.get("/api/products", (request, response) => {
+    response.status(200).send(products)
+})
+
+// Filter Implementation
+app.get("/api/products/filter", (request, response) => {
+    const product = products.filter((product) => product.name === "Juice")
+    response.status(200).send(product)
+})
+
+app.post("/api/product/create", (request, response) => {
+    const { body } = request;
+    const newProduct = {id: products[products.length - 1].id + 1, ...body}
+    products.push(newProduct)
+    return response.status(201).send(newProduct)
 })
 
 app.listen(PORT, () => {
-    console.log(`Server Running at port: ${PORT}`);
+    console.log(`The server running at port: ${PORT}`)
 })
