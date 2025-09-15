74a58e4385451b4dd064ea15728fc10545c82e52
jemadari <hassanjemadari@gmail.com>
2025-09-15 15:27:42 +0300

    update on brokerService functions

--- insurance-broker-app/src/services/brokerService.js
+++ insurance-broker-app/src/services/brokerService.js
@@ -1,13 +1,46 @@
-import { createBroker, getBrokersByCompanyId } from '../models/db.js';
+import { createBroker, getBrokersByCompanyId, getCompanyById } from '../models/db.js';
 
 export const getBrokersByCompanyIdService = async (companyId) => {
-    // TODO: Fetch and return brokers for a company
-    // Validate companyId exists first?
-    throw new Error('Not implemented yet');
+    if(!companyId)
+    {
+        throw new Error('company Id is required')
+    }
+
+    const company = await getCompanyById(companyId);
+    if(!company)
+    {
+        throw new Error(`Company with ID: ${companyId} not found`);
+    }
+
+    const brokers = await getBrokersByCompanyId(companyId);
+    return brokers
 };
 
 export const createBrokerService = async (name, companyId) => {
-    // TODO: Create a new broker for a company
-    // Validate name and companyId
-    throw new Error('Not implemented yet');
+    if(!name || name.trim === '')
+    {
+        throw new Error('broker name is required');
+    }
+
+    if(!companyId)
+    {
+        throw new Error('Company Id is required')
+    }
+
+    const company = await getCompanyById(companyId);
+    if(!company)
+    {
+        throw new Error(`Company with ID: ${companyId} not found`);
+    }
+
+    const brokers = await getBrokersByCompanyId(companyId);
+    const existingBroker = brokers.find((broker) => broker.name === name)
+
+    if(existingBroker)
+    {
+        throw new Error(`Broker with the name: ${name} already exists for this company`);
+    }
+
+    const newBroker = await createBroker(name, companyId);
+    return newBroker
 };
